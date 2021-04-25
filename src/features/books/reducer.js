1d1845154acd4dc0bc4ca036c9be53c5338694fa
Yu Shi <yushiys95@gmail.com>
2021-04-25 16:18:57 -0400

    Done.

---
+++ src/features/books/reducer.js
@@ -0,0 +1,11 @@
+// src/reducers.js
+import { combineReducers } from "redux";
+import authorsReducer from "./features/authors/authorsSlice";
+import booksReducer from "./features/books/booksSlice";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer,
+});
+
+export default rootReducer;
